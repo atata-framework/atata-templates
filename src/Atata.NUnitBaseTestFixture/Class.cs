c5e1d1d84b9d404cf199f2a1751ff686f0b10c02
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-24 11:30:28 +0300

    #3 Add "Atata NUnit Base Test Fixture" item template

---
+++ src/Atata.NUnitBaseTestFixture/Class.cs
@@ -0,0 +1,30 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace $rootnamespace$
+{
+    [TestFixture]
+    public class $safeitemrootname$
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Find information about AtataContext set-up on https://atata-framework.github.io/getting-started/#set-up.
+            AtataContext.Configure().
+                //UseChrome().
+                //    WithArguments("start-maximized").
+                //UseBaseUrl("SITE_URL").
+                UseCulture("en-us").
+                UseNUnitTestName().
+                AddNUnitTestContextLogging().
+                LogNUnitError().
+                Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current.CleanUp();
+        }
+    }
+}
---
+++ src/Atata.NUnitBaseTestFixture/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.NUnitBaseTestFixture")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("23e1eb9d-6ec3-4acd-afa1-e3eb41ee10ca")]
