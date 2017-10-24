59cd73e74bb81122c73ee053c144e9588d249ba1
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-24 12:26:38 +0300

    #4 Rename "Atata Test Project" project template to "Atata NUnit Test Project (.NET Framework)"

---
+++ src/Atata.NUnitTestProject.NetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.NUnitTestProject.NetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("b2567662-ac63-4c32-8626-30d598f46a4a")]
