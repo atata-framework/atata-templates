3007b7d6f7812b80569b465828e2b49527224413
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-24 15:27:53 +0300

    #5 Add "Atata NUnit Test Project (.NET Core)" project template

---
+++ src/Atata.NUnitTestProject.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.NUnitTestProject.NetCore")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("9a5eb5e6-72c5-4708-a27c-86bcb5a4c696")]
