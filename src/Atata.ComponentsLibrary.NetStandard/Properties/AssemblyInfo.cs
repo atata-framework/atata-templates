253fe3fd67adc896c8c94390976c40eac9b483d3
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-25 11:45:35 +0300

    #8 Add "Atata Components Library (.NET Standard)" project template

---
+++ src/Atata.ComponentsLibrary.NetStandard/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.ComponentsLibrary.NetStandard")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("a51b20fe-6da8-4b1d-bfa5-817e6e178169")]
