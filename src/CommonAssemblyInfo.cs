30c4103aff7889de8c0dad162ad68ff62e629057
YevgeniyShunevych <eshunevich@gmail.com>
2017-11-27 15:11:19 +0200

    Increase project version to 0.15.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© Yevgeniy Shunevych 2017")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.15.0")]
-[assembly: AssemblyFileVersion("0.15.0")]
+[assembly: AssemblyVersion("0.15.1")]
+[assembly: AssemblyFileVersion("0.15.1")]
