b1b0d49326306a5d5d871d1a55b6c048e9a41f67
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-06-05 09:49:27 +0300

    Increase project version to 0.17.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Yevgeniy Shunevych")]
 [assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2017")]
+[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2018")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.16.0")]
-[assembly: AssemblyFileVersion("0.16.0")]
+[assembly: AssemblyVersion("0.17.0")]
+[assembly: AssemblyFileVersion("0.17.0")]
