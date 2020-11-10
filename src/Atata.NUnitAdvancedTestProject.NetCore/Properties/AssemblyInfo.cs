98cfea5e835fd583b995c35f4bf6c4b9a719d78c
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-11-10 14:46:49 +0200

    #28 Add "Atata NUnit Advanced Test Project (.NET Core)" project template

---
+++ src/Atata.NUnitAdvancedTestProject.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.NUnitAdvancedTestProject.NetCore")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("45534220-0b3c-4a2a-a72e-71ba0cde926b")]
---
+++ src/Atata.NUnitAdvancedTestProject.NetCore/SetUpFixture.cs
@@ -0,0 +1,25 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            string environmentAlias = TestContext.Parameters.Get("TestEnvironment", "local");
+            string driverAlias = TestContext.Parameters.Get("DriverAlias", DriverAliases.Chrome);
+
+            // Find information on AtataContext set-up on https://atata.io/getting-started/#set-up
+            // Find information on Atata JSON configuration on https://github.com/atata-framework/atata-configuration-json
+            // NOTE: Driver configuration is required.
+            //       For example, install Selenium.WebDriver.ChromeDriver NuGet package.
+            AtataContext.GlobalConfiguration
+                .ApplyJsonConfig<AtataConfig>()
+                .ApplyJsonConfig<AtataConfig>(environmentAlias: environmentAlias)
+                .UseDriver(driverAlias);
+        }
+    }
+}
