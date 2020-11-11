e7aab49799abe709ee2ffb7f0ff499336cc678fb
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-11-11 16:51:33 +0200

    #36 Add SetUpFixture.cs to "Atata NUnit Basic Test Project (.NET Core)" project template

---
+++ src/Atata.NUnitBasicTestProject.NetCore/SetUpFixture.cs
@@ -0,0 +1,24 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            // Find information about AtataContext set-up on https://atata.io/getting-started/#set-up
+            // NOTE: Driver configuration is required.
+            //       For example, install Selenium.WebDriver.ChromeDriver NuGet package.
+            AtataContext.GlobalConfiguration
+                .UseChrome()
+                    .WithArguments("start-maximized")
+                .UseBaseUrl("https://atata.io/")
+                .UseCulture("en-US")
+                .UseAllNUnitFeatures()
+                .Build();
+        }
+    }
+}
