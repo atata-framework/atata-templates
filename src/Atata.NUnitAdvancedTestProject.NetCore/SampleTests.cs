cd94255d5f79e733459af2dc9b82779c850fdf7b
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-02-23 16:16:13 +0200

    #43 Add SampleTests class to project templates

---
+++ src/Atata.NUnitAdvancedTestProject.NetCore/SampleTests.cs
@@ -0,0 +1,15 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    public class SampleTests : UITestFixture
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Go.To<OrdinaryPage>()
+                .PageTitle.Should.Contain("Atata");
+        }
+    }
+}
