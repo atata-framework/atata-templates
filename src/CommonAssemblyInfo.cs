ebfbb806cacc7730f3aaff4e34cac33b02890cc9
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-02-09 19:45:45 +0100

    Increment copyright year to 2023

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Yevgeniy Shunevych")]
 [assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2022")]
+[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2023")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.6.0")]
