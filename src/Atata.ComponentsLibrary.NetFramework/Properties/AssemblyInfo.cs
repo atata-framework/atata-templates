e06e7a4c9c489218bd29d40d41208e1ac543c973
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-09-01 17:18:40 +0200

    #73 Remove project templates for old .NET versions

--- src/Atata.ComponentsLibrary.NetFramework/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Atata.ComponentsLibrary.NetFramework")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("f560f771-7f79-40e7-b542-0af2e7548fb8")]
--- src/Atata.NUnitBasicTestProject.NetFramework/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Atata.NUnitBasicTestProject.NetFramework")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("b2567662-ac63-4c32-8626-30d598f46a4a")]
