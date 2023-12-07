0627e024802d2d441406d65402b1061e22730a20
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-12-07 10:17:42 +0100

    Increase project version to 2.13.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© Yevgeniy Shunevych 2023")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.12.0")]
-[assembly: AssemblyFileVersion("2.12.0")]
+[assembly: AssemblyVersion("2.13.0")]
+[assembly: AssemblyFileVersion("2.13.0")]
