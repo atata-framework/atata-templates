6714b33e856d49bbe83dce1c5e2716ee92da3e1c
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-12-07 09:14:46 +0100

    #83 Upgrade .NET 7 project templates to .NET 8

--- src/Atata.NUnitAdvancedTestProject.Net8/Properties/AssemblyInfo.cs
+++ src/Atata.NUnitAdvancedTestProject.Net8/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Atata.NUnitAdvancedTestProject.Net7")]
+[assembly: AssemblyTitle("Atata.NUnitAdvancedTestProject.Net8")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("45534220-0b3c-4a2a-a72e-71ba0cde926b")]
--- src/Atata.NUnitBasicTestProject.Net8/Properties/AssemblyInfo.cs
+++ src/Atata.NUnitBasicTestProject.Net8/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Atata.NUnitBasicTestProject.Net7")]
+[assembly: AssemblyTitle("Atata.NUnitBasicTestProject.Net8")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("9a5eb5e6-72c5-4708-a27c-86bcb5a4c696")]
