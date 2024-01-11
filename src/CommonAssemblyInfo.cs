ebe8ae00cedc08073a3b16201095436178979e94
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-01-11 15:58:04 +0100

    Increase project version to 2.14.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Yevgeniy Shunevych")]
 [assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2023")]
+[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2024")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.13.0")]
-[assembly: AssemblyFileVersion("2.13.0")]
+[assembly: AssemblyVersion("2.14.0")]
+[assembly: AssemblyFileVersion("2.14.0")]
