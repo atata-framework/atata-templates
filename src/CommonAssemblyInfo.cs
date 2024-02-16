b867d2adccafbee166a4323b12da726adf8232b5
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-02-16 09:47:22 +0100

    Increase project version to 2.14.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© Yevgeniy Shunevych 2024")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.14.0")]
-[assembly: AssemblyFileVersion("2.14.0")]
+[assembly: AssemblyVersion("2.14.1")]
+[assembly: AssemblyFileVersion("2.14.1")]
