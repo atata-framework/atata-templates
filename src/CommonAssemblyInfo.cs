3b105acadf1ab9506bff73e9545db749629a7d75
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2026-01-22 09:17:33 +0100

    Change copyright year

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Yevgeniy Shunevych")]
 [assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2017-2025")]
+[assembly: AssemblyCopyright("© Yevgeniy Shunevych 2017-2026")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("3.8.0")]
